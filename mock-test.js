1170df5838cc7f797e905f373488c36b1ecf1bf5
肖鹏飞 <xiaopf@300624.cn>
2024-11-05 14:57:25 +0800

    chore: 测试代码

--- mock-test.js
+++ mock-test.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
-const filePath = '@wefly-rs/copy-folder.darwin-x64.node';
-fs.stat(filePath, (err, stats) => {
-  if (!err && stats.isFile()) {
-    console.log('文件存在。');
-  } else {
-    console.log('文件不存在。');
-  }
-});
+// const filePath = '@wefly-rs/copy-folder.darwin-x64.node';
+// fs.stat(filePath, (err, stats) => {
+//   if (!err && stats.isFile()) {
+//     console.log('文件存在。');
+//   } else {
+//     console.log('文件不存在。');
+//   }
+// });
 
 console.log(process.cwd());
+
+const files = fs.readdirSync('.');
+files.forEach(file => {
+  console.log(file);
+});
 
 console.log("test passed !!!");
